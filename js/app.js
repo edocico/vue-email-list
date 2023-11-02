f202d8f4799c7d337de9c5fe70c2db9d0916f435
edocico <138378767+edocico@users.noreply.github.com>
2023-11-02 16:36:40 +0100

    fix v-if , add style , add maxemailsnumber

--- js/app.js
+++ js/app.js
@@ -5,17 +5,18 @@ createApp({
     return {
       message: "Here's the email list:",
       mailList: [],
+      maxEmailsNumber: 10,
     };
   },
   methods: {
     getTenEmail() {
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < this.maxEmailsNumber; i++) {
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((res) => {
             //console.log(res, res.data);
-            const mailaddress = res.data.response;
-            this.mailList.push(mailaddress);
+            const mailAddress = res.data.response;
+            this.mailList.push(mailAddress);
           });
       }
     },
