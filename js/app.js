3f7713920dae7571ae1d6d3b33d1a20ae425b35f
edocico <138378767+edocico@users.noreply.github.com>
2023-11-02 15:41:48 +0100

    print mail list only when it's fully loaded

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ createApp({
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((res) => {
-            console.log(res, res.data);
+            //console.log(res, res.data);
             const mailaddress = res.data.response;
             this.mailList.push(mailaddress);
           });
