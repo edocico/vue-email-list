9b76e0f5c4c25ad94d569add8637c3c6e9270b00
edocico <138378767+edocico@users.noreply.github.com>
2023-11-02 15:31:54 +0100

    print emails in a list

--- js/app.js
+++ js/app.js
@@ -3,8 +3,24 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      message: "Here's the email list:",
+      mailList: [],
     };
   },
-  methods: {},
+  methods: {
+    getTenEmail() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((res) => {
+            console.log(res, res.data);
+            const mailaddress = res.data.response;
+            this.mailList.push(mailaddress);
+          });
+      }
+    },
+  },
+  created() {
+    this.getTenEmail();
+  },
 }).mount("#app");
